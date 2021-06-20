1318e886d624f8fede3c6ac62ebfc7ca2ca05404
Agata Szulejko <amoranska@poczta.wp.pl>
2021-06-20 17:33:04 +0200

    All articles links in one variable

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const titleClickHandler = function(event){
+{
+  const titleClickHandler = function(event){
   event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
@@ -36,4 +37,54 @@ const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
+}
+
+
+// Part 6.4 - Dynamic Title links
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+
+let html = '';
+
+  /* for each article */
+const articles = document.querySelectorAll(optArticleSelector);
+for (let article of articles){
+
+    /* get the article id */
+  const articleId = article.getAttribute('id');
+
+    /* find the title element */
+    /* get the title from the title element */
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+  /* create HTML of the link */
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+  console.log(linkHTML);
+
+  /* insert link into titleList */
+  //titleList.innerHTML = titleList.innerHTML + linkHTML;
+  //titleList.insertAdjacentHTML('beforeend', linkHTML);
+
+  /* insert link into html variable */
+      html = html + linkHTML;
+      console.log(html);
+  }
+
+titleList.innerHTML = html;
+
+
+}
+
+generateTitleLinks();
+
 }
